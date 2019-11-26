3636523376c48a5dfc371cf7e0a235462c403890
josephkim583 <josephkim583@gmail.com>
2019-11-25 18:16:06 -0600

    debug print message please print something

--- Go/src/GO/gameAdmin.java
+++ Go/src/GO/gameAdmin.java
@@ -9,18 +9,25 @@ import java.net.Socket;
 public class gameAdmin {
     public static void main(String[] args) throws Exception {
         Game referee = new Game();
+        System.out.println("it's reading config");
+
         ConfigReader config = new ConfigReader();
         JSONArray winners = new JSONArray();
+        System.out.println("it's starting to connect port: " + config.port());
 
         //open sockets
         InetAddress addr = InetAddress.getByName(config.ipAddress());
         ServerSocket ss = new ServerSocket(config.port(), 50, addr);
         Socket s = ss.accept();
+        System.out.println("Connected to port: " + ss.getLocalPort());
+
 
         //register player 1
         Player localPlayer = new Player();
         referee.playerOne = localPlayer;
         referee.registerPlayer();
+        System.out.println("It is registering player one at least");
+
 
         //register player 2
         ProxyPlayer proxyPlayer = new ProxyPlayer(s);
