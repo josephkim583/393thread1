d13accf0df4d2e67bcff3c9e5572dd8618e6ad17
josephkim583 <josephkim583@gmail.com>
2019-11-25 19:24:14 -0600

    jar filed moved correctly

--- Go/src/GO/Game.java
+++ Go/src/GO/Game.java
@@ -103,15 +103,14 @@ public class Game implements GameInterface {
         while (!gameEnded) {
 //            System.out.println(boardHistory.get(0).printBoard());
             if (currentStoneColor.equals("B")){
-                pass();
-//                String playerOneMove = playerOne.makeADumbMove(boardHistory);
-//                if (playerOneMove.equals("pass")){
-//                    pass();
-//                }
-//                else{
-//                    Point playerOneMovePoint = new Point(playerOneMove);
-//                    makeMove(playerOneMovePoint);
-//                }
+                String playerOneMove = playerOne.makeADumbMove(boardHistory);
+                if (playerOneMove.equals("pass")){
+                    pass();
+                }
+                else{
+                    Point playerOneMovePoint = new Point(playerOneMove);
+                    makeMove(playerOneMovePoint);
+                }
             }
             else {
                 JSONArray makeAMoveArray = new JSONArray();
